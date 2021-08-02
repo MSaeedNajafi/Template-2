110ba44991ba7e17f12d3693b7a98ff44316d7bf
Saeed <najafisaeed@gmail.com>
2021-08-02 23:28:42 +0200

    v1

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import COL from "../Colors";
 
 export const ServicesContainer = styled.div`
-  height: 800px;
+  height: 1000px;
   display: flex;
   flex-direction: column;
   justify-content: center;
